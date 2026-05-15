5edd60b52f671aaa0ea514e0542618ebfa3e90b2
camiloolea20077 <112830645+camiloolea20077@users.noreply.github.com>
2026-05-15 07:50:28 -0500

    feat: inicia proyecto

---
+++ src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind, primeng',
+          },
+          darkModeSelector: '.dark',
+        }
+      }
+    })
+  ]
+};
